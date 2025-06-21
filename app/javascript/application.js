646448ee3df9c607499fa1eff06917c4492b47ab
Nishi12Y <nishimura.y12@gmail.com>
2025-06-22 06:23:21 +0900

    bootstrap導入

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,6 @@
 // Entry point for the build script in your package.json
+//= require jquery3
+//= require popper
+//= require bootstrap-sprockets
 import "@hotwired/turbo-rails"
 import "./controllers"
