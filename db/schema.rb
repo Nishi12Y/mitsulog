f57c0990515de0e754ffa97abede89cc63a46171
Nishi12Y <nishimura.y12@gmail.com>
2025-06-22 08:08:59 +0900

    add:sorceryの導入とuserテーブルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_06_21_230145) do
+ActiveRecord::Schema[7.2].define(version: 2025_06_21_230454) do
+  create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "email", null: false
+    t.string "crypted_password"
+    t.string "salt"
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+  end
 end
