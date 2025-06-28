07e62779c2ef9cf07f026221c08632a2466a0584
Nishi12Y <nishimura.y12@gmail.com>
2025-06-28 13:50:19 +0900

    add：logout機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,7 @@ Rails.application.routes.draw do
   # login_pathが生成される
   get 'login' , to: 'user_sessions#new'
   post 'login' , to: 'user_sessions#create'
+  delete 'logout' , to: 'user_sessions#destroy'
 
   resources :users, only: [:new, :create]
   # resources :diaries, only: [:index, :new, :create]
